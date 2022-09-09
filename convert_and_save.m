687037afda6a44b90d9a1776c8da198e3c28c217
Pedro Porto Buarque de Gusmão <pedropgusmao@gmail.com>
2022-09-09 14:04:12 +0100

    Almost there

--- convert_and_save.m
+++ convert_and_save.m
@@ -1,6 +1,5 @@
 function convert_and_save(data_dir)
 fileList = dir([data_dir filesep '*.mat']);
-
 for file_num =1:length(fileList)
     datafile = [fileList(file_num).folder filesep fileList(file_num).name];
     loaded_vars = load(datafile);
@@ -9,17 +8,27 @@ for file_num =1:length(fileList)
     for var_idx = 1:length(loaded_vars_names)
         this_var_name = loaded_vars_names{var_idx};
         this_var = loaded_vars.(this_var_name);
-        first_row = this_var(2,:);
-        for j = 1:length(first_row) % Find which rows are strings
-            if isa(first_row{j},'string')
-                for i = 1:length(this_var)
-                    this_var{i,j} = convertStringsToChars(this_var{i, j});
+        if isa(this_var, 'table') % header is not the first line
+            header = this_var.Properties.VariableNames;
+            for j = 1:length(header)
+                if isa(this_var{1,j}, 'string')
+                    this_var.(header{j}) = char(this_var.(header{j}));
+                end
+            end
+
+        elseif isa(this_var, 'cell') % header is not the first line
+            first_row = this_var(2,:);
+            for j = 1:length(first_row) % Find which rows are strings
+                if isa(first_row{j},'string')
+                    for i = 1:length(this_var)
+                        this_var{i,j} = convertStringsToChars(this_var{i,j});
+                    end
                 end
             end
         end
         loaded_vars.(this_var_name) = this_var;
     end
-    save(datafile,'-struct', 'loaded_vars', '-v7') 
+    save(datafile,'-struct', 'loaded_vars', '-v7.3') 
     disp(['Done proccessing file: ' datafile])
 end
 end
--- operanet.py
+++ operanet.py
@@ -1,8 +1,7 @@
-import h5py
 import pandas as pd
+import mat73
 from pandas.core.frame import DataFrame
 from pathlib import Path
-from scipy.io import loadmat
 from typing import Dict, List
 from numpy import ndarray
 
@@ -19,14 +18,7 @@ def read_uwb(path_to_file: Path) -> DataFrame:
     return pd.read_csv(path_to_file, header=1)
 
 
-def get_header_from_ndarray(columns_row: ndarray):
-    header = []
-    for column_name in columns_row:
-        header.append(column_name[0])
-    return header
-
-
-def read_mat(path_to_file: Path, modality: str) -> DataFrame:
+def read_mat(path_to_file: Path) -> DataFrame:
     """Reads MAT files for Kinect
 
     Args:
@@ -36,14 +28,8 @@ def read_mat(path_to_file: Path, modality: str) -> DataFrame:
     Returns:
         DataFrame: Pandas DataFrame with extracted data.
     """
-    if modality not in ["Kinect", "PWR"]:
-        pass
-    try:
-        # CSI files were created using matlab v7.3
-        # so h5py is necessary
-        tmp_mat = loadmat(path_to_file)[modality]
-    except:
-        with h5py.File(path_to_file, "r") as f:
-            tmp_mat = f[modality]
-    header = get_header_from_ndarray(tmp_mat[0])
-    return DataFrame(tmp_mat[1:], columns=header)
+    # CSI files were created using matlab v7.3
+    # so h5py is necessary
+    tmp_dict = mat73.loadmat(path_to_file, use_attrdict=True)
+    k, v = tmp_dict.popitem()
+    return pd.DataFrame(v[1:], columns=v[0])
